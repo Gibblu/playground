91994a64e1e385706a93829fb503298c5cabf034
Noman Butt <20231100099@kps.edu.pk>
2025-06-12 03:28:47 +0000

    test2

--- Main.kt
+++ Main.kt
@@ -1,3 +1,10 @@
 fun main() {
     println("Welcome to the Kotlin Playground!")
+    val test = Test()
+}
+
+class Test(val test1: String = "t1", val test2: String = "t2") {
+    init {
+        println("Test class initialized with values: $test1 and $test2")
+    }
 }
